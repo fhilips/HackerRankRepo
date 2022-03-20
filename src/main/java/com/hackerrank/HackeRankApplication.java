056793bbadd644dfb3d6eca5f9e5b516b2805e23
fhilips <fhilips.souza@gmail.com>
2022-03-20 11:23:38 -0300

    add initial files

---
+++ src/main/java/com/hackerrank/HackeRankApplication.java
@@ -0,0 +1,13 @@
+package com.hackerrank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HackeRankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HackeRankApplication.class, args);
+	}
+
+}
