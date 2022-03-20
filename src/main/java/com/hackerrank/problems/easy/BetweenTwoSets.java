d0d20e5cab116d49ccea9bb581ab95f36b0d2126
fhilips <fhilips.souza@gmail.com>
2022-03-20 16:47:03 -0300

    BetweenTwoSets

---
+++ src/main/java/com/hackerrank/problems/easy/BetweenTwoSets.java
@@ -0,0 +1,46 @@
+package com.hackerrank.problems.easy;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
+/**
+ * https://www.hackerrank.com/challenges/between-two-sets/problem?isFullScreen=true
+ *
+ *
+ */
+public class BetweenTwoSets {
+
+    public static int getTotalX(List<Integer> a, List<Integer> b) {
+        int sum = 0;
+        int min = a.stream().min(Comparator.naturalOrder()).get();
+
+        for(int i = min; i <= b.get(b.size()-1); i++){
+            int m = i;
+            boolean b1 = a.stream().allMatch(x -> m % x == 0);
+            boolean b2 = b.stream().allMatch(x -> x % m == 0);
+            if(b1 && b2){
+                sum++;
+            }
+        }
+
+        return sum;
+    }
+
+}
+
+class SolutionBetweenTwoSets {
+    public static void main(String[] args) throws IOException {
+
+        List<Integer> arr = Arrays.asList(2, 6);
+        List<Integer> brr = Arrays.asList(24, 36);
+
+//        List<Integer> arr = Arrays.asList(2, 4);
+//        List<Integer> brr = Arrays.asList(16, 32, 96);
+
+        int total = BetweenTwoSets.getTotalX(arr, brr);
+        System.out.println(total);
+    }
+}
+
