9f68120e484d0145e7e4c34e75d8ed8ca662ab2e
fhilips <fhilips.souza@gmail.com>
2022-03-20 12:37:13 -0300

    SolveMeFirst

---
+++ src/main/java/com/hackerrank/problems/easy/SolveMeFirst.java
@@ -0,0 +1,19 @@
+package com.hackerrank.problems.easy;
+
+public class SolveMeFirst {
+
+    static int solveMeFirst(int a, int b) {
+        return a + b;
+    }
+
+    public static void main(String[] args) {
+
+        int a = 5, b = 6;
+
+        int sum;
+        sum = solveMeFirst(a, b);
+
+        System.out.println(sum); // 6
+    }
+
+}
